3e870cca74f73fd8c018570e3dbc14aa69276c39
Michael Hobbs <mh@expa.com>
2014-03-14 14:20:44 -0700

    autopep8 and pep8 compliance

--- docs/conf.py
+++ docs/conf.py
@@ -170,21 +170,21 @@ htmlhelp_basename = '{{ project_name }}doc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', '{{ project_name }}.tex', u'{{ project_name }} Documentation',
-   u'ChangeToMyName', 'manual'),
+    ('index', '{{ project_name }}.tex', u'{{ project_name }} Documentation',
+     u'ChangeToMyName', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,9 +227,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', '{{ project_name }}', u'{{ project_name }} Documentation',
-   u'ChangeToMyName', '{{ project_name }}', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', '{{ project_name }}', u'{{ project_name }} Documentation',
+     u'ChangeToMyName', '{{ project_name }}', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
--- project_name/urls.py
+++ project_name/urls.py
@@ -6,15 +6,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+                       url(r'^$', TemplateView.as_view(template_name='base.html')),
 
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+                       # Examples:
+                       # url(r'^$', '{{ project_name }}.views.home', name='home'),
+                       # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
